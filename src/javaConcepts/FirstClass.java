34e2283e933781f60c3d0aa35349da6a4365a523
Shivanand <shivanand1437@gmail.com>
2023-07-05 18:10:27 +0530

    this my first commit

---
+++ src/javaConcepts/FirstClass.java
@@ -0,0 +1,11 @@
+package javaConcepts;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello All");
+		System.out.print("Welcome To Java Learning..");
+	}
+
+}
